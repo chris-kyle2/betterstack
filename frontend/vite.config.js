e4af7e618656740f74b7c40e756c6897233d6bfa
chris-kyle2 <22ms0010@iitism.ac.in>
2025-05-15 14:38:04 +0530

    Deploy to vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -14,10 +14,15 @@ export default defineConfig({
   resolve: {
     extensions: ['.js', '.jsx', '.json'],
     alias: {
-      '@': path.resolve(__dirname, './src')
+      '@': path.resolve(__dirname, './src'),
+      'pages': path.resolve(__dirname, './src/pages')
     }
   },
   build: {
+    commonjsOptions: {
+      include: [/node_modules/],
+      extensions: ['.js', '.jsx']
+    },
     rollupOptions: {
       input: {
         main: path.resolve(__dirname, 'index.html')
